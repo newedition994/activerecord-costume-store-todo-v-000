22090aa7918368360ba60543737daee964b358b6
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-18 17:43:42 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.date :opening_date
       t.date :closing_date
+      t.string :description
     end
   end
 end
